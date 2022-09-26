5956a68782e435330a46287ef8c47d1fb727b78b
uddin-nayem <uddin.nayem@gmail.com>
2022-09-26 17:00:13 +0600

    display cart data with calculate

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import Product from '../Product/Product';
+import './Cart.css'
 
 const Cart = ({cart}) => {
     console.log(cart);
+    // cart price add 
+    let total = 0;
+    let shipping = 0;
+    for (const product of cart) {
+        total = total + product.price
+        shipping = shipping + product.shipping;
+    }
+    const tax = parseFloat((total * 0.1).toFixed(2));
+    // console.log(typeof tax);
+    const grandTotal = total + shipping + tax
     return (
-        <div>
+        <div className='cart'>
             <h3>order cart summery</h3>
             <p>Selected item : {cart.length}</p>
+            <p>Total Price:${ total}</p>
+            <p>Total Shipping:${ shipping}</p>
+            <p>Tax:${ tax}</p>
+            <h3>Grand Total:${ grandTotal.toFixed(2)}</h3>
         </div>
     );
 };
